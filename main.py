1fe0513d90545b1c2936b3c356bcf4bb10b47c10
Jupsimar Singh <82681171+jupsimar@users.noreply.github.com>
2022-10-18 13:19:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests 
+
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+# Store API response in a variable.
+response_dict = r.json()
+print(f"Total repositories: {response_dict['total_count']}")
+
+# Explore information about the repositories.
+repo_dicts = response_dict['items']
+print(f"Repositories returned: {len(repo_dicts)}")
+
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")
+    print("\n")
+
+
+import urllib.request
+import json
+
+url = f'https://api.github.com/rate_limit'
+
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+print(result)
